c08c5404c045867e45a4acc50803472d28671bcc
junsik <dlwnstlr1010@naver.com>
2023-12-23 12:32:02 +0900

    Intent를 이용한 다음화면으로 넘어가기 & 전 화면으로 돌아오기

--- app/src/main/java/com/example/intentpractice/OtherActivity.kt
+++ app/src/main/java/com/example/intentpractice/OtherActivity.kt
@@ -1,11 +1,23 @@
 package com.example.intentpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.intentpractice.databinding.ActivityOtherBinding
+
 
 class OtherActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOtherBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_other)
+        binding = ActivityOtherBinding.inflate((layoutInflater))
+        setContentView(binding.root)
+
+        binding.moveToFirst.setOnClickListener {
+            val myintent2 = Intent(this, MainActivity::class.java)
+            startActivity(myintent2)
+        }
     }
 }
